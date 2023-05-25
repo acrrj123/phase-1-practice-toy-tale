a18b584ee34fc43e33e503d1c156a3b1ef3f8510
Anna Cole <acrrj123@gmail.com>
2023-05-25 14:34:16 -0500

    made card

--- src/index.js
+++ src/index.js
@@ -13,3 +13,26 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   });
 });
+
+// Fetch Andy's Toys from the server. With the response data, make a <div class="card"> for each toy and add it to the toy-collection div.
+
+fetch('http://localhost:3000/toys')
+.then(resp => resp.json())
+//.then(data => console.log(data))
+.then(data => makeCard(data))
+
+function makeCard(arrayToys) {
+  let divToys = document.querySelector('#toy-collection');
+  arrayToys.forEach(toy => {
+    let card = document.createElement('div');
+    card.className = 'card';
+    divToys.appendChild(card)
+  })
+}
+
+// card.innerHTML = `
+    // <h2>${toy.name}</h2>
+    // <img src="${toy.image}" class="toy-avatar">
+    // <p>${likes}</p>
+    // <button class="like-btn" id=${id}>Like ❤️</button> 
+    //

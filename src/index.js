e0ffecaab068477d193c7ccc8d237e1ab7effddd
Anna Cole <acrrj123@gmail.com>
2023-05-27 14:55:18 -0500

    Final version

--- src/index.js
+++ src/index.js
@@ -18,12 +18,12 @@ document.addEventListener("DOMContentLoaded", () => {
 
 fetch('http://localhost:3000/toys')
 .then(resp => resp.json())
-.then(data => data.forEach(toy => makeCard(toy)))
+.then(data => data.forEach(toy => renderToys(toy)))
 
-// With the response data, make a <div class="card"> for each toy and add it to the toy-collection div. Then, add toy info to the card.
+// With the response data, render the toys in the DOM, by making a card <div class="card"> for each toy and adding it to the toy-collection div. Then, add toy info into the cards.
 
-function makeCard(toy) {
-    let divToys = document.querySelector('#toy-collection');
+function renderToys(toy) {
+    let toyContainer = document.querySelector('#toy-collection');
     let card = document.createElement('div');
     card.className = 'card';
 
@@ -35,64 +35,57 @@ function makeCard(toy) {
     
     const likeBtn = card.querySelector('.like-btn')
     likeBtn.addEventListener('click', () => {
-      toy.likes+= 1
-      card.querySelector('p').textContent = toy.likes
-      updateLikes(toy)
+      let p = card.querySelector('p')
+      p.textContent = `${toy.likes+= 1} likes`
+      updateLikes(toy.id, toy.likes)
     })
-  
-    divToys.appendChild(card)
+    toyContainer.appendChild(card)
   }
 
 // Add a new toy. When a user submits the toy form, send a POST request to add the new toy to Andy's Toy Collection. If the post is successful, the toy should be added to the DOM without reloading the page.
 
 const form = document.querySelector('.add-toy-form')
 
-form.addEventListener('submit', handleSubmit)
-
-function handleSubmit (e) {
+form.addEventListener('submit', e => {
   e.preventDefault()
   //console.log(e.target.name)
-  let toyObj = {
+  let newToyObj = {
     name: e.target.name.value,
     image: e.target.image.value,
     likes: 0
   }
-  makeCard(toyObj)
-  post(toyObj)
+  renderNewToy(newToyObj)
   e.target.reset()
-}
+})
 
-// Then, make the POST request
-function post(toyObj) {
+// Then, make the POST request to send to the server the new card.
+function renderNewToy(newToyObj) {
   const configObj = {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
       Accept: 'application/json'
     },
-    body: JSON.stringify(toyObj)
-  };
+    body: JSON.stringify(newToyObj)
+  }
   fetch('http://localhost:3000/toys', configObj)
   .then(res => res.json())
-  .then(toy => console.log(toy))
+  .then(newToy => renderToys(newToy))
 }
 
-// The Patch request
-function updateLikes(toyObj) {
-  fetch(`http://localhost:3000/toys/${toyObj.id}`, {
+// Then, make the PATCH request to update the number of likes in the server.
+function updateLikes(id, newNumberOfLikes) {
+  fetch(`http://localhost:3000/toys/${id}`, {
     method: 'PATCH',
     headers: {
       'Content-Type': 'application/json',
       Accept: 'application/json'
     },
-    body: JSON.stringify(toyObj)
+    body: JSON.stringify({likes: newNumberOfLikes})
   })
   .then(resp => resp.json())
-  .then(toy => console.log(toy))
+  .then(updatedToy => console.log(updatedToy))
 }
 
 
 
-
-
-    
